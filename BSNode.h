4e2f251588627ab234a9ef2c53948f27d962fba7
Carles Gradolí Montagud <cgramon@upv.edu.es>
2023-12-22 17:47:45 +0100

    Añadida implementación de la clase BSNode

---
+++ BSNode.h
@@ -0,0 +1,28 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+using namespace std;
+
+template <typename T>
+class BSNode{
+ public:
+  T elem;                //Elemento de tipo T almacenado en el Nodo
+  BSNode<T>* left;       //Puntero al Nodo sucesor izquierdo
+  BSNode<T>* right;      //Puntero al Nodo sucesor derecho
+
+  //Método constructor
+  BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr){
+    this -> elem = elem;
+    this -> left = left;
+    this -> right = right;
+  }
+
+  friend ostream&operator<<(ostream&out, const BSNode<T>&bsn){
+    out << bsn.elem;
+    return out;
+  }
+};
+
+#endif
